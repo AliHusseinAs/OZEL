1bdfd9b154759f6edc20c2084d7cd2ce730d287f
Ali Hussein <ali@alMBPalkhasbAli.home>
2025-03-07 19:56:05 +0300

    m

---
+++ SODEFEND/src/main/java/SODEFEND/SODEFEND/RepositoryLayer/RepoInter.java
@@ -0,0 +1,13 @@
+package SODEFEND.SODEFEND.RepositoryLayer;
+
+import SODEFEND.SODEFEND.RegisterLayer.RegisterModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RepoInter extends JpaRepository<RegisterModel, Long> {
+    boolean  existsByUserName(String userName);
+    Optional<RegisterModel> deleteByUserName(String userName);
+    Optional<RegisterModel> findByUserName(String userName);
+    Optional<RegisterModel> findByUserNameAndMail(String userName, String mail);
+}
